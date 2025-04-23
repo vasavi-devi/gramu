34f3349661b4e60a5703d7b55c804b1ac9278f11
vasavi <vasavi9113@gmail.com>
2025-04-23 12:53:04 +0000

    change port

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def home():
     return "Hello, Vasavi! Your Python app is running on port 50000 🎉"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=50000)
+    app.run(host='0.0.0.0', port=40000)
 
